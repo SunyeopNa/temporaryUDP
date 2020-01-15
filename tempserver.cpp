aefc4d9b835f92b077c3669391d177c9342bdf78
Nasunyeop <sy.na@alcherainc.com>
2020-01-15 13:46:09 +0900

    q
    q

--- tempserver.cpp
+++ tempserver.cpp
@@ -16,9 +16,6 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0}; 
     char *hello = "Hello from server"; 
 
-    Json::Value root;
-    Json::Reader reader;
-
        
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
