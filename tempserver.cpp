24a2f62b2351ac85f7e918a13f5101170449e6a4
Nasunyeop <sy.na@alcherainc.com>
2020-01-15 14:20:45 +0900

    nmvm
    nmv

--- tempserver.cpp
+++ tempserver.cpp
@@ -1,81 +1,77 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <iostream>
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 9333 
-int main(int argc, char const *argv[])
-{
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = { 0 };
+/*
+ *   C++ sockets on Unix and Windows
+ *   Copyright (C) 2002
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-	for (int i = 0; i < 1024; i++)
-	{
-		buffer[i] = 'F';
-	}
-	char *hello = "Hello from server";
+#include "PracticalSocket.h"  // For Socket, ServerSocket, and SocketException
+#include <iostream>           // For cerr and cout
+#include <cstdlib>            // For atoi()
 
-	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+using namespace std;
 
-	// Forcefully attaching socket to the port 4444 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-		&opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+const unsigned int RCVBUFSIZE = 32;    // Size of receive buffer
 
-	// Forcefully attaching socket to the port 8080 
-	if (bind(server_fd, (struct sockaddr *)&address,
-		sizeof(address)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
+void HandleTCPClient(TCPSocket *sock); // TCP client handling function
 
-	while (1)
-	{
-        std::cout << "Wait for New Message."<<std::endl;
-		if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-			(socklen_t*)&addrlen)) < 0)
-		{
-			perror("accept");
-			exit(EXIT_FAILURE);
-		}
+int main(int argc, char *argv[]) {
+  if (argc != 2) {                     // Test for correct number of arguments
+    cerr << "Usage: " << argv[0] << " <Server Port>" << endl;
+    exit(1);
+  }
 
-        std::cout << "Message Accepted."<<std::endl;
+  unsigned short echoServPort = atoi(argv[1]);  // First arg: local port
 
-        std::cout << "read Message."<<std::endl;
-		valread = read(new_socket, buffer, 1024);
+  try {
+    TCPServerSocket servSock(echoServPort);     // Server Socket object
+  
+    for (;;) {   // Run forever
+      HandleTCPClient(servSock.accept());       // Wait for a client to connect
+    }
+  } catch (SocketException &e) {
+    cerr << e.what() << endl;
+    exit(1);
+  }
+  // NOT REACHED
 
-		for (int i = 0; i < 1024; i++)
-		{
-			std::cout << buffer[i];
-		}
-		std::cout << std::endl;
+  return 0;
+}
 
-		//printf("%s : %s\n","mesage_recieved",buffer ); 
-		//send(new_socket , hello , strlen(hello) , 0 ); 
-		//printf("Hello message sent\n"); 
-	}
-	return 0;
+// TCP client handling function
+void HandleTCPClient(TCPSocket *sock) {
+  cout << "Handling client ";
+  try {
+    cout << sock->getForeignAddress() << ":";
+  } catch (SocketException e) {
+    cerr << "Unable to get foreign address" << endl;
+  }
+  try {
+    cout << sock->getForeignPort();
+  } catch (SocketException e) {
+    cerr << "Unable to get foreign port" << endl;
+  }
+  cout << endl;
+
+  // Send received string and receive again until the end of transmission
+  char echoBuffer[RCVBUFSIZE];
+  int recvMsgSize;
+  while ((recvMsgSize = sock->recv(echoBuffer, RCVBUFSIZE)) > 0) { // Zero means
+                                                         // end of transmission
+    // Echo message back to client
+    sock->send(echoBuffer, recvMsgSize);
+  }
+  delete sock;
 }
