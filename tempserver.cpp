4f3bb8e2cae8773a27a023052d521462b0052336
Nasunyeop <sy.na@alcherainc.com>
2020-01-15 13:13:57 +0900

    w
    w

--- tempserver.cpp
+++ tempserver.cpp
@@ -52,7 +52,7 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE); 
     } 
     valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
+    printf("$s : %s\n","mesage_recieved",buffer ); 
     send(new_socket , hello , strlen(hello) , 0 ); 
     printf("Hello message sent\n"); 
     return 0; 
